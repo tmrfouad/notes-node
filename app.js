27283a0579415c6dfa93c58398aeb349dc353870
tmrfouad <tmrfouad@gmail.com>
2018-11-07 16:18:28 +0200

    2018-11-07 04:18

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ console.log('Starting app.js...');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var command = process.argv[2];
+var argv = yargs.argv;
+var command = argv._[0];
 console.log('Command: ', command);
-console.log(process.argv);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
 
 if (command === 'add') {
-  console.log('adding new note...');
+  notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
   console.log('showing a list of notes...');
 } else if (command === 'remove') {
